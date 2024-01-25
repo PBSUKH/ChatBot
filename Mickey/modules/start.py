95a72232835107e77ce6b9bd4dca70b6775d6359
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-01-25 16:29:26 +0530

    Create start.py

---
+++ Mickey/modules/start.py
@@ -0,0 +1,39 @@
+# Don't remove This Line From Here. Tg: @Dev_Arora_0981 | @DevArora0981
+# Github :- Devarora-0981 | Devarora2604
+
+import random
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from config import IMG, OWNER_USERNAME, STICKER
+from Mickey import MickeyBot
+from Mickey.database.chats import add_served_chat
+from Mickey.database.users import add_served_user
+from Mickey.modules.helpers import PNG_BTN
+
+
+@MickeyBot.on_cmd("start")
+async def ping(_, message: Message):
+    await message.reply_sticker(sticker=random.choice(STICKER))
+    start = datetime.now()
+    loda = await message.reply_photo(
+        photo=random.choice(IMG),
+        caption="𝐖ᴀɪᴛɪɴɢ 𝐁ᴀʙʏ🥀",
+    )
+    try:
+        await message.delete()
+    except:
+        pass
+
+    ms = (datetime.now() - start).microseconds / 1000
+    await loda.edit_text(
+        text=f""("📲") </b>",
+        reply_markup=InlineKeyboardMarkup(PNG_BTN),
+    )
+    if message.chat.type == ChatType.PRIVATE:
+        await add_served_user(message.from_user.id)
+    else:
+        await add_served_chat(message.chat.id)
